0c4d224ec6467d9e4fdd14f10745151769c4a8b2
rickyriosp <riki.rios91@gmail.com>
2023-12-24 14:47:46 -0500

    display product list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  //templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
