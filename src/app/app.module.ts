0827004a49917cb1afc61e016842dd78f96fdb17
rickyriosp <riki.rios91@gmail.com>
2024-01-10 23:36:04 -0500

    integrating online template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [ProductService],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -5,8 +5,9 @@ import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  //templateUrl: './product-list.component.html',
-  templateUrl: './product-list-table.component.html',
+  // templateUrl: './product-list.component.html',
+  // templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent implements OnInit {
