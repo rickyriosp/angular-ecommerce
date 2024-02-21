84662ff4c1fd5712cdf73670ecd70b56a75c591f
RiosPena <ricky.rios@capgemini.com>
2024-02-21 11:14:19 -0500

    updated production build config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: true,
-  shopApiUrl: 'https://localhost:8443/api',
+  shopApiUrl: 'http://localhost:8080/api',
   stripePublishableKey: 'pk_test_51OiiG6KMw4UXD7ARN7DmRN7LcpVx35P3nEOvREA4teBFHyhfL7PXRZjqFLt06vwdIvswidezfkJJE2LSz4rPMyHE00XD2VIarr',
 };
